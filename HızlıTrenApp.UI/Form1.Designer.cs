2ce5c8276495be84d3c5731f41c866cd2971fe61
Semih Güzel <semih_guzel@outlook.com>
2019-01-26 21:52:18 +0300

    Data class'ları tek bir klasörde toplandı(deneme push).

--- HızlıTrenApp.UI/Form1.Designer.cs
+++ HızlıTrenApp.UI/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- HızlıTrenApp.UI/Form1.cs
+++ HızlıTrenApp.UI/Form1.cs
@@ -16,5 +16,10 @@ namespace HızlıTrenApp.UI
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
