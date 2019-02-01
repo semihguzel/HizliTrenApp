05883c4d8e883bbd6e2c0696cd3dfc2e234814ed
Sevgi AKTEN <sevgiakten@gmail.com>
2019-02-02 00:49:30 +0200

    Koltuk seçimi formuna Yolcu Bilgileri eklendi

--- HızlıTrenApp.UI/frmKoltukSecimi.Designer.cs
+++ HızlıTrenApp.UI/frmKoltukSecimi.Designer.cs
@@ -28,36 +28,259 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.flpKoltuklar = new System.Windows.Forms.FlowLayoutPanel();
-            this.SuspendLayout();
-            // 
-            // flpKoltuklar
-            // 
-            this.flpKoltuklar.Location = new System.Drawing.Point(63, 157);
-            this.flpKoltuklar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.flpKoltuklar.Name = "flpKoltuklar";
-            this.flpKoltuklar.Size = new System.Drawing.Size(1076, 378);
-            this.flpKoltuklar.TabIndex = 1;
-            // 
-            // frmKoltukSecimi
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.flpKoltuklar);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "frmKoltukSecimi";
-            this.Padding = new System.Windows.Forms.Padding(30, 92, 30, 31);
-            this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.None;
-            this.Style = MetroFramework.MetroColorStyle.Red;
-            this.Text = "Koltuk Seçimi";
-            this.TextAlign = System.Windows.Forms.VisualStyles.HorizontalAlign.Right;
-            this.ResumeLayout(false);
+			this.flpKoltuklar = new System.Windows.Forms.FlowLayoutPanel();
+			this.grpYolcuBilgileri = new System.Windows.Forms.GroupBox();
+			this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+			this.txtAd = new MetroFramework.Controls.MetroTextBox();
+			this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+			this.txtSoyad = new MetroFramework.Controls.MetroTextBox();
+			this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+			this.txtTcNo = new MetroFramework.Controls.MetroTextBox();
+			this.lbl = new MetroFramework.Controls.MetroLabel();
+			this.rdbErkek = new System.Windows.Forms.RadioButton();
+			this.rdbKadin = new System.Windows.Forms.RadioButton();
+			this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+			this.txtEmail = new MetroFramework.Controls.MetroTextBox();
+			this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+			this.txtTelefon = new MetroFramework.Controls.MetroTextBox();
+			this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+			this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+			this.btnOdemeyeGec = new MetroFramework.Controls.MetroButton();
+			this.grpYolcuBilgileri.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// flpKoltuklar
+			// 
+			this.flpKoltuklar.Location = new System.Drawing.Point(38, 63);
+			this.flpKoltuklar.Name = "flpKoltuklar";
+			this.flpKoltuklar.Size = new System.Drawing.Size(717, 246);
+			this.flpKoltuklar.TabIndex = 1;
+			// 
+			// grpYolcuBilgileri
+			// 
+			this.grpYolcuBilgileri.Controls.Add(this.btnOdemeyeGec);
+			this.grpYolcuBilgileri.Controls.Add(this.metroComboBox1);
+			this.grpYolcuBilgileri.Controls.Add(this.dateTimePicker1);
+			this.grpYolcuBilgileri.Controls.Add(this.rdbKadin);
+			this.grpYolcuBilgileri.Controls.Add(this.rdbErkek);
+			this.grpYolcuBilgileri.Controls.Add(this.txtTelefon);
+			this.grpYolcuBilgileri.Controls.Add(this.txtEmail);
+			this.grpYolcuBilgileri.Controls.Add(this.txtTcNo);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel7);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel6);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel4);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel5);
+			this.grpYolcuBilgileri.Controls.Add(this.lbl);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel3);
+			this.grpYolcuBilgileri.Controls.Add(this.txtSoyad);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel2);
+			this.grpYolcuBilgileri.Controls.Add(this.txtAd);
+			this.grpYolcuBilgileri.Controls.Add(this.metroLabel1);
+			this.grpYolcuBilgileri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.grpYolcuBilgileri.Location = new System.Drawing.Point(38, 326);
+			this.grpYolcuBilgileri.Name = "grpYolcuBilgileri";
+			this.grpYolcuBilgileri.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.grpYolcuBilgileri.Size = new System.Drawing.Size(717, 198);
+			this.grpYolcuBilgileri.TabIndex = 2;
+			this.grpYolcuBilgileri.TabStop = false;
+			this.grpYolcuBilgileri.Text = "Yolcu Bilgileri";
+			// 
+			// metroLabel1
+			// 
+			this.metroLabel1.AutoSize = true;
+			this.metroLabel1.Location = new System.Drawing.Point(35, 22);
+			this.metroLabel1.Name = "metroLabel1";
+			this.metroLabel1.Size = new System.Drawing.Size(26, 19);
+			this.metroLabel1.TabIndex = 0;
+			this.metroLabel1.Text = "Ad";
+			// 
+			// txtAd
+			// 
+			this.txtAd.Location = new System.Drawing.Point(68, 17);
+			this.txtAd.Name = "txtAd";
+			this.txtAd.Size = new System.Drawing.Size(142, 23);
+			this.txtAd.TabIndex = 1;
+			// 
+			// metroLabel2
+			// 
+			this.metroLabel2.AutoSize = true;
+			this.metroLabel2.Location = new System.Drawing.Point(16, 65);
+			this.metroLabel2.Name = "metroLabel2";
+			this.metroLabel2.Size = new System.Drawing.Size(45, 19);
+			this.metroLabel2.TabIndex = 0;
+			this.metroLabel2.Text = "Soyad";
+			// 
+			// txtSoyad
+			// 
+			this.txtSoyad.Location = new System.Drawing.Point(68, 61);
+			this.txtSoyad.Name = "txtSoyad";
+			this.txtSoyad.Size = new System.Drawing.Size(142, 23);
+			this.txtSoyad.TabIndex = 1;
+			// 
+			// metroLabel3
+			// 
+			this.metroLabel3.AutoSize = true;
+			this.metroLabel3.Location = new System.Drawing.Point(16, 107);
+			this.metroLabel3.Name = "metroLabel3";
+			this.metroLabel3.Size = new System.Drawing.Size(46, 19);
+			this.metroLabel3.TabIndex = 0;
+			this.metroLabel3.Text = "TC No";
+			// 
+			// txtTcNo
+			// 
+			this.txtTcNo.Location = new System.Drawing.Point(68, 103);
+			this.txtTcNo.Name = "txtTcNo";
+			this.txtTcNo.Size = new System.Drawing.Size(142, 23);
+			this.txtTcNo.TabIndex = 1;
+			// 
+			// lbl
+			// 
+			this.lbl.AutoSize = true;
+			this.lbl.Location = new System.Drawing.Point(8, 145);
+			this.lbl.Name = "lbl";
+			this.lbl.Size = new System.Drawing.Size(53, 19);
+			this.lbl.TabIndex = 0;
+			this.lbl.Text = "Cinsiyet";
+			// 
+			// rdbErkek
+			// 
+			this.rdbErkek.AutoSize = true;
+			this.rdbErkek.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.rdbErkek.Location = new System.Drawing.Point(75, 145);
+			this.rdbErkek.Name = "rdbErkek";
+			this.rdbErkek.Size = new System.Drawing.Size(62, 21);
+			this.rdbErkek.TabIndex = 2;
+			this.rdbErkek.TabStop = true;
+			this.rdbErkek.Text = "Erkek";
+			this.rdbErkek.UseVisualStyleBackColor = true;
+			// 
+			// rdbKadin
+			// 
+			this.rdbKadin.AutoSize = true;
+			this.rdbKadin.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.rdbKadin.Location = new System.Drawing.Point(143, 143);
+			this.rdbKadin.Name = "rdbKadin";
+			this.rdbKadin.Size = new System.Drawing.Size(62, 21);
+			this.rdbKadin.TabIndex = 2;
+			this.rdbKadin.TabStop = true;
+			this.rdbKadin.Text = "Kadın";
+			this.rdbKadin.UseVisualStyleBackColor = true;
+			// 
+			// metroLabel4
+			// 
+			this.metroLabel4.AutoSize = true;
+			this.metroLabel4.Location = new System.Drawing.Point(256, 22);
+			this.metroLabel4.Name = "metroLabel4";
+			this.metroLabel4.Size = new System.Drawing.Size(87, 19);
+			this.metroLabel4.TabIndex = 0;
+			this.metroLabel4.Text = "Doğum Tarihi";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(350, 17);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+			this.dateTimePicker1.TabIndex = 3;
+			// 
+			// metroLabel5
+			// 
+			this.metroLabel5.AutoSize = true;
+			this.metroLabel5.Location = new System.Drawing.Point(298, 65);
+			this.metroLabel5.Name = "metroLabel5";
+			this.metroLabel5.Size = new System.Drawing.Size(41, 19);
+			this.metroLabel5.TabIndex = 0;
+			this.metroLabel5.Text = "Email";
+			// 
+			// txtEmail
+			// 
+			this.txtEmail.Location = new System.Drawing.Point(350, 61);
+			this.txtEmail.Name = "txtEmail";
+			this.txtEmail.Size = new System.Drawing.Size(142, 23);
+			this.txtEmail.TabIndex = 1;
+			// 
+			// metroLabel6
+			// 
+			this.metroLabel6.AutoSize = true;
+			this.metroLabel6.Location = new System.Drawing.Point(289, 103);
+			this.metroLabel6.Name = "metroLabel6";
+			this.metroLabel6.Size = new System.Drawing.Size(50, 19);
+			this.metroLabel6.TabIndex = 0;
+			this.metroLabel6.Text = "Telefon";
+			// 
+			// txtTelefon
+			// 
+			this.txtTelefon.Location = new System.Drawing.Point(350, 103);
+			this.txtTelefon.Name = "txtTelefon";
+			this.txtTelefon.Size = new System.Drawing.Size(142, 23);
+			this.txtTelefon.TabIndex = 1;
+			// 
+			// metroLabel7
+			// 
+			this.metroLabel7.AutoSize = true;
+			this.metroLabel7.Location = new System.Drawing.Point(277, 147);
+			this.metroLabel7.Name = "metroLabel7";
+			this.metroLabel7.Size = new System.Drawing.Size(67, 19);
+			this.metroLabel7.TabIndex = 0;
+			this.metroLabel7.Text = "Ek Hizmet";
+			// 
+			// metroComboBox1
+			// 
+			this.metroComboBox1.FormattingEnabled = true;
+			this.metroComboBox1.ItemHeight = 23;
+			this.metroComboBox1.Location = new System.Drawing.Point(350, 143);
+			this.metroComboBox1.Name = "metroComboBox1";
+			this.metroComboBox1.Size = new System.Drawing.Size(142, 29);
+			this.metroComboBox1.TabIndex = 4;
+			// 
+			// btnOdemeyeGec
+			// 
+			this.btnOdemeyeGec.Location = new System.Drawing.Point(548, 141);
+			this.btnOdemeyeGec.Name = "btnOdemeyeGec";
+			this.btnOdemeyeGec.Size = new System.Drawing.Size(109, 31);
+			this.btnOdemeyeGec.TabIndex = 11;
+			this.btnOdemeyeGec.Text = "Ödemeye Geç";
+			// 
+			// frmKoltukSecimi
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 547);
+			this.Controls.Add(this.grpYolcuBilgileri);
+			this.Controls.Add(this.flpKoltuklar);
+			this.Name = "frmKoltukSecimi";
+			this.ShadowType = MetroFramework.Forms.MetroForm.MetroFormShadowType.None;
+			this.Style = MetroFramework.MetroColorStyle.Red;
+			this.Text = "Koltuk Seçimi";
+			this.TextAlign = System.Windows.Forms.VisualStyles.HorizontalAlign.Right;
+			this.grpYolcuBilgileri.ResumeLayout(false);
+			this.grpYolcuBilgileri.PerformLayout();
+			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.FlowLayoutPanel flpKoltuklar;
+		private System.Windows.Forms.GroupBox grpYolcuBilgileri;
+		private System.Windows.Forms.RadioButton rdbErkek;
+		private MetroFramework.Controls.MetroTextBox txtTcNo;
+		private MetroFramework.Controls.MetroLabel lbl;
+		private MetroFramework.Controls.MetroLabel metroLabel3;
+		private MetroFramework.Controls.MetroTextBox txtSoyad;
+		private MetroFramework.Controls.MetroLabel metroLabel2;
+		private MetroFramework.Controls.MetroTextBox txtAd;
+		private MetroFramework.Controls.MetroLabel metroLabel1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.RadioButton rdbKadin;
+		private MetroFramework.Controls.MetroTextBox txtTelefon;
+		private MetroFramework.Controls.MetroTextBox txtEmail;
+		private MetroFramework.Controls.MetroLabel metroLabel6;
+		private MetroFramework.Controls.MetroLabel metroLabel4;
+		private MetroFramework.Controls.MetroLabel metroLabel5;
+		private MetroFramework.Controls.MetroComboBox metroComboBox1;
+		private MetroFramework.Controls.MetroLabel metroLabel7;
+		private MetroFramework.Controls.MetroButton btnOdemeyeGec;
 	}
 }
